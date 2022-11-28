5e15cf3ae2496039c5f0ed72396e34eb5a21f64e
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2022-11-28 20:12:45 +0100

    connect to mongo db and allows cors

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(function(req, res, next) {
 
 
 //my connection string from mongo db: mongodb+srv://userdb:<password>@cluster0.xbzxxb7.mongodb.net/?retryWrites=true&w=majority
+
 // database stuff
 const uri = "mongodb+srv://userdb:<password>@cluster0.xbzxxb7.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(uri, {
@@ -27,4 +28,19 @@ mongoose.connect(uri, {
   console.log("MongoDB connected")
 })
 .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+//routes
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+// respond with "hello world" when a GET request is made to the homepage
+app.listen(3000,()=>{
+  console.log('Listning at port 3000');
+})
+
 
